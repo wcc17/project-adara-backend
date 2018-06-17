0739c59baf3387bed46302719267e0cb9a3647c1
Christian Curry <christian.curry17@gmail.com>
2018-06-17 18:11:25 -0400

    added info to Device model to display in front end

--- src/main/java/com/projectadara/Controller/DeviceController.java
+++ src/main/java/com/projectadara/Controller/DeviceController.java
@@ -13,15 +13,25 @@ public class DeviceController {
 
     @RequestMapping("/deviceList")
     public List<Device> getDeviceList(@RequestParam(value="customerId") long customerId) {
+        System.out.println("Retrieving list of devices");
+
         List<Device> deviceList = new ArrayList<Device>();
 
-        //TODO: query by customerId, building dummy list for now
-        deviceList.add(new Device(customerId, 0, "Camera0"));
-        deviceList.add(new Device(customerId, 1, "Camera1"));
-        deviceList.add(new Device(customerId, 2, "Camera2"));
-        deviceList.add(new Device(customerId, 3, "Camera3"));
-        deviceList.add(new Device(customerId, 4, "Camera4"));
-        deviceList.add(new Device(customerId, 5, "Camera5"));
+        //TODO: query by customerId passed in here, building dummy list for now
+        deviceList.add(new Device(customerId, 0, "Camera0", Device.CAMERA, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
+        deviceList.add(new Device(customerId, 1, "Camera1", Device.CAMERA, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+        deviceList.add(new Device(customerId, 2, "Camera2", Device.CAMERA, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"));
+
+        deviceList.add(new Device(customerId, 6, "Microphone1", Device.MICROPHONE, null));
+        deviceList.add(new Device(customerId, 7, "Microphone2", Device.MICROPHONE, null));
+
+        deviceList.add(new Device(customerId, 9, "Sensor0", Device.SENSOR, null));
+        deviceList.add(new Device(customerId, 10, "Sensor1", Device.SENSOR, null));
+        deviceList.add(new Device(customerId, 11, "Sensor2", Device.SENSOR, null));
+        deviceList.add(new Device(customerId, 12, "Sensor3", Device.SENSOR, null));
+        deviceList.add(new Device(customerId, 13, "Sensor4", Device.SENSOR, null));
+
+        deviceList.add(new Device(customerId, 14, "Lock1", Device.LOCK, null));
 
         return deviceList;
     }
--- src/main/java/com/projectadara/Model/Device.java
+++ src/main/java/com/projectadara/Model/Device.java
@@ -1,14 +1,28 @@
 package com.projectadara.Model;
 
 public class Device {
+    public static final int CAMERA = 1;
+    public static final int MICROPHONE = 2;
+    public static final int SENSOR = 3;
+    public static final int LOCK = 4;
+
     long customerId;
     int deviceId;
     String deviceName;
+    int deviceType;
+    String deviceUrl;
 
-    public Device(long customerId, int deviceId, String deviceName) {
+    //TODO: temporary constructor until retrieving stuff from db
+    public Device(long customerId, int deviceId, String deviceName, int deviceType, String deviceUrl) {
         this.customerId = customerId;
         this.deviceId = deviceId;
         this.deviceName = deviceName;
+        this.deviceType = deviceType;
+        this.deviceUrl = deviceUrl;
+    }
+
+    public long getCustomerId() {
+        return customerId;
     }
 
     public int getDeviceId() {
@@ -17,5 +31,13 @@ public class Device {
 
     public String getDeviceName() {
         return deviceName;
+    }
+
+    public int getDeviceType() {
+        return deviceType;
+    }
+
+    public String getDeviceUrl() {
+        return deviceUrl;
     }
 }
